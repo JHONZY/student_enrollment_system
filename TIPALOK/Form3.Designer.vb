85587a0fb1db1c9130bd7322d552955d37f4379a
ACER TRAVELMATE <ACER TRAVELMATE@DSWD>
2023-05-24 08:22:15 +0800

    Add project files.

---
+++ TIPALOK/Form3.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        username = New TextBox()
+        password = New TextBox()
+        Button2 = New Button()
+        LinkLabel1 = New LinkLabel()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Sans Serif Collection", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ActiveCaptionText
+        Label1.Location = New Point(49, 18)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(273, 67)
+        Label1.TabIndex = 0
+        Label1.Text = "REGISTER"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = SystemColors.ButtonHighlight
+        Label2.ImageAlign = ContentAlignment.MiddleLeft
+        Label2.Location = New Point(49, 101)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(84, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Username:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.ForeColor = SystemColors.ButtonHighlight
+        Label3.ImageAlign = ContentAlignment.MiddleLeft
+        Label3.Location = New Point(53, 142)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(80, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Password:"
+        ' 
+        ' username
+        ' 
+        username.Location = New Point(139, 101)
+        username.Name = "username"
+        username.Size = New Size(165, 23)
+        username.TabIndex = 3
+        ' 
+        ' password
+        ' 
+        password.Location = New Point(139, 138)
+        password.Name = "password"
+        password.PasswordChar = "*"c
+        password.Size = New Size(165, 23)
+        password.TabIndex = 4
+        ' 
+        ' Button2
+        ' 
+        Button2.AutoSize = True
+        Button2.BackColor = Color.Cyan
+        Button2.Cursor = Cursors.Hand
+        Button2.FlatAppearance.BorderSize = 0
+        Button2.FlatAppearance.MouseOverBackColor = Color.Green
+        Button2.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.ForeColor = SystemColors.ActiveCaptionText
+        Button2.Location = New Point(139, 180)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(107, 31)
+        Button2.TabIndex = 19
+        Button2.Text = "Register"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Font = New Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        LinkLabel1.ForeColor = SystemColors.ButtonHighlight
+        LinkLabel1.LinkColor = Color.White
+        LinkLabel1.Location = New Point(253, 186)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(51, 20)
+        LinkLabel1.TabIndex = 20
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Log In"
+        ' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DarkSlateGray
+        ClientSize = New Size(375, 241)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Button2)
+        Controls.Add(password)
+        Controls.Add(username)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Cursor = Cursors.Hand
+        ForeColor = SystemColors.ButtonHighlight
+        Name = "Form3"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form3"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents username As TextBox
+    Friend WithEvents password As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ TIPALOK/Form3.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Net
+Imports MySql.Data.MySqlClient
+
+Public Class Form3
+
+    Dim co As String = "server=localhost;port=3306;user=root;password=;database=sample_crud"
+    Dim conn As New MySqlConnection(co)
+    Dim cmd As MySqlCommand
+    Dim i As Integer
+
+    Sub Register()
+        Try
+            conn.Open()
+
+            ' Check if user already exists
+            Dim checkCmd As New MySqlCommand("SELECT COUNT(*) FROM `user` WHERE `USERNAME` = @USERNAME", conn)
+            checkCmd.Parameters.AddWithValue("@USERNAME", username.Text)
+            Dim userCount As Integer = Convert.ToInt32(checkCmd.ExecuteScalar())
+
+            If (username.Text = "" Or password.Text = "") Then
+                MsgBox("Fields cannot be empty!")
+            ElseIf (userCount > 0) Then
+                MsgBox("User already exists")
+            Else
+                ' Insert new user
+                Dim insertCmd As New MySqlCommand("INSERT INTO `user` (`USERNAME`, `PASSWORD`) VALUES (@USERNAME, @PASSWORD)", conn)
+                insertCmd.Parameters.AddWithValue("@USERNAME", username.Text)
+                insertCmd.Parameters.AddWithValue("@PASSWORD", password.Text)
+
+                Dim i As Integer = insertCmd.ExecuteNonQuery()
+                If (i > 0) Then
+                    MessageBox.Show("Registered Successfully!", "ALERT")
+                    username.Clear()
+                    password.Clear()
+                Else
+                    MessageBox.Show("Unsuccessful", "ALERT")
+                End If
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Register()
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Form2.Show()
+        Me.Hide()
+
+    End Sub
+End Class
